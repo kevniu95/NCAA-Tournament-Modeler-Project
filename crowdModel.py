0243ac5d58b99fa07510e57c0f070e9a05d33307
kevniu95 <kniu@uchicago.edu>
2022-02-18 00:45:38 -0600

    initial commit

---
+++ crowdModel.py
@@ -0,0 +1,277 @@
+import numpy as np
+import math
+import bracketImporter
+from abc import ABC, abstractmethod
+import pandas as pd
+
+class CrowdPickGetter():
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def getRounds(self):
+        pass
+
+class MockCrowdPickGetter(CrowdPickGetter):
+    def __init__(self, input = 'WhoPickedWhom - Mock.xlsx'):
+        self.input = input
+
+    def getCrowdPicks(self):
+        allCols = []
+        df = pd.read_excel(self.input,sheet_name = 'Sheet2', header = None)
+        for col in df.columns:
+            newCol = df[col].to_numpy()
+            allCols.append(newCol)
+        return allCols
+
+class Round():
+    def __init__(self, roundNum, teamsProbDictionary):
+        self.roundNum = roundNum
+        self.teamsProbDictionary = teamsProbDictionary
+    
+    def __str__(self):
+        return f"Round {self.roundNum} entries: {self.teamsProbDictionary}"
+    
+    def __repr__(self):
+        return f"Round {self.roundNum} entries: {self.teamsProbDictionary}"
+
+class Rounds():
+    def __init__(self, pickGetter):
+        self.rounds = [None] * 6
+        self.pickGetter = pickGetter
+    
+    def fillRoundPicks(self):
+        rawRoundInfo = self.pickGetter.getCrowdPicks()
+        teams = rawRoundInfo[0]
+        roundCtr = 0
+        for arr in rawRoundInfo[1:]:
+            roundDict = dict(zip(teams, arr))
+            self.rounds[roundCtr] = Round(roundCtr, roundDict)
+            roundCtr += 1
+            
+class PotentialWinnerGetter():
+    '''
+    This class is built to interact directly with crowdBracketEntry id numbers
+    '''
+    def __init__(self):
+        self.rounds = [Round]
+        # Array of dictionaries
+            # Each ditionary, or entry in array, will represent a 'round'
+            # Where key:value pairs are teams : % of people selecting them to win in this round
+        
+        # Rounds will be numbered with 1 being championship, or top layer
+        # 6 will be bottom layer
+        pass
+    
+    def getPotentialWinners(self, roundID):
+        """
+        Champ game is '0'
+            -Stored in in rounds[0]
+        F4 is '00' and '01' 
+            -Stored in rounds[1]
+        E8 is '000', '001', '010', and '011'
+            -Stored in rounds[2]
+        """
+        potentialWinnerList = []
+        currentRound = self.determineRound(roundID)
+        for team, chance in currentRound:
+            matchToRoundIDPart = (currentRound + 1)
+            if team[:matchToRoundIDPart] == roundID:
+                potentialWinnerList.append((team, chance))
+        return potentialWinnerList
+
+    def determineRound(self, roundID):
+        return self.rounds[len(roundID) - 1]
+
+class crowdBracket():
+    def __init__(self):
+        self.bracketList = [None] * 64
+    
+    def instantiateBracket(self):
+        self._instantiateFirstEntry()
+        for arrayIndex in range(2, 64):
+            baseIndex = arrayIndex // 2 # The "last" round that preceded this one
+            baseEntryGameID = self.bracketList[baseIndex].id
+            if arrayIndex % 2 == 0:
+                currGameID = baseEntryGameID + "0"
+            else:
+                currGameID = baseEntryGameID + "1" 
+            self.bracketList[arrayIndex] = crowdBracketEntry(currGameID, self, arrayIndex)
+        
+    def _instantiateFirstEntry(self):
+        self.bracketList[1] = crowdBracketEntry("0", self, 1)
+        
+    def simulateTourney(self):
+        """
+        Want: A function that can apply to each node no matter what
+
+        Will apply this function on each element in the list self.bracketList
+
+        1. Check to see if bracket entry already has a winner - if so, pass
+        2. Else
+            a. Pick a winner for this game using the .pickWinner() method belonging
+                to the bracketEntry
+            b. Separate function that tumbles down and fills out winners
+        """
+        for entry in self.bracketList[1:]:
+            if entry.choseWinner:
+                pass
+            else:
+                entry.pickWinner()
+                self.tumbleWinnerDown(entry)
+        
+    def tumbleWinnerDown(self, bracketEntry):
+        currGameID, winner, arrayIndex = bracketEntry.getInfo()
+        tmpWinner = winner
+        tmpWinner = tmpWinner.replace(currGameID, "", 1)
+
+        while len(tmpWinner) > 1:
+            currLetter = tmpWinner[0]
+            arrayIndex = 2 * arrayIndex + int(currLetter)
+            self.bracketList[arrayIndex]._winner = winner
+            tmpWinner = tmpWinner[1:]
+        
+class crowdBracketEntry():
+    def __init__(self, id, bracket, arrayIndex):
+        self.id = id
+        self.arrayIndex = arrayIndex
+        self.bracket = bracket
+        self._winner = None
+        self._potentialWinners = []
+        self._choseWinner = False
+    
+    def __repr__(self):
+        return f"Game ID: {self.id}\nBracketArrayIndex: {self.arrayIndex}\nWinner: {self._winner}\nPotential Winners: {self._potentialWinners}"
+
+    def __str__(self):
+        return f"Game ID: {self.id}\nBracketArrayIndex: {self.arrayIndex}\nWinner: {self._winner}\nPotential Winners: {self._potentialWinners}"
+
+    @property
+    def potentialWinners(self):
+        idLength = len(self.id)
+        if len(self._potentialWinners) == 0:
+            self._potentialWinners = PotentialWinnerGetter.getPotentialWinners(self.id)
+        return self._potentialWinners
+    
+    @potentialWinners.setter
+    def potentialWinners(self):
+        print("Sorry, not allowed to set the winners, check back in implementation of crowdBracketEntry class!")
+        return
+
+    @property
+    def winner(self):
+        if self._winner is None:
+            print("Need to pick the winner for this round first!")
+        else:
+            return self._winner
+    
+    @winner.setter
+    def winner(self, value):
+        if value.startswith(self.id):
+            self._winner = value
+            self._choseWinner = True
+        else:
+            print(f"Team {value} is not allowed to win game {self.id}!")
+        
+    @property
+    def choseWinner(self):
+        return self._choseWinner
+
+    @choseWinner.setter
+    def choseWinner(self):
+        if self._choseWinner:
+            print("Winner for this game has already been determined!")
+        else:
+            print("Not allowed to choose winner directly. Please run the pickWinner() method on this object.")
+    
+    def pickWinner(self):
+        currPotentialWinners = self._potentialWinners
+        currPotentialWinners.sort(key = lambda x: x[1], reverse = True)
+        assert currPotentialWinners[0][1] > currPotentialWinners[1][1]
+        unifDraw = np.random.uniform(0, 1)
+        self._winner = self.pickWinnerGivenDraw(currPotentialWinners, unifDraw)
+        self._choseWinner = True
+        
+    def pickWinnerGivenDraw(self, potentialWinners, draw):
+        cdf = 0
+        for entry in potentialWinners:
+            prob = entry[0]
+            team = entry[1]
+            cdf += prob
+            if draw < cdf:
+                return team
+        print("Uh-oh, no team was chosen to win in this bracket entry!")
+        return None
+
+    def getInfo(self):
+        return self.id, self.winner, self.arrayIndex
+
+# teamImport = bracketImporter.bracketImporterBracketologyESPN()
+# teams = teamImport.teams
+# print(teams)
+
+# test = crowdBracket()
+# test.instantiateBracket()
+
+# print()
+# test.bracketList[1].winner = '0111111'
+# test.bracketList[2].winner = '0000000'
+# test.tumbleWinnerDown(test.bracketList[1])
+# test.tumbleWinnerDown(test.bracketList[2])
+
+# for num, i in enumerate(test.bracketList):
+#     print(i)
+#     print()
+
+
+'''TODO: 
+
+Keep PotentialWinnerGetter, b/c that does something separate from just holding the rounds data
+
+Rounds data structure that holds the rounds, where each round has:
+    -Key with round number (0 is champ game, 1 is semis .... 6 is first round)
+    -Value with Round dicts
+        
+Data structure that holds one round:
+    -Each entry is {Team : pct of winning in that rd}
+    -Team can be an actual Team object to make things easier
+
+Teams data structure with
+    -Team IDs that make everything happen (like structure 000000 for Duke in 2019)
+    -The normal team name so humans can read and understand
+
+Larger bracket structure that integrates many BracketEntries
+    -This is where, at very least, need to connect all bracket entries
+    -Fill backwards once winner chosen in specific entry
+    -Traverse down tree, checking to see if entires are filled
+      
+
+2/17 
+DID:
+DOWNSTREAM
+-In CrowdBracket class (i.e., went further downstream in terms of actually generating simulation)
+    -Implemented TumbleWinnerDown() in CrowdBracket class
+    -Implemented InstantiateTourney()
+
+UPSTREAM
+-Built bracketImporter module to handle generating team ID's based on actual bracket 
+    -(Webscraping to get teams their proper IDs)
+-Built Rounds and Round data structures 
+    -Rounds holds individual Round in array
+    -Round has round number, and team odds of winning in that specific round
+
+
+-
+
+TODO:
+Link up teams from bracketImporter to teams in Rounds information
+    -Or put another way, need to know what actual team IDs are now that we have
+        the "who picked whom" information from the internet
+    -Should be final piece keeping us from simulating "the people" brackets
+    -The Rounds information should ultimately have not just Team ID, but actual
+        team object
+
+Will also need to implement an actual subclass of CrowdPickGetter that will
+    work on the actual ESPN web format
+
+''' 
---
+++ trueModel.py
@@ -0,0 +1,212 @@
+# True Model
+
+from multiprocessing.sharedctypes import Value
+import numpy as np
+from abc import ABC, abstractmethod
+import random
+import math
+
+class PredictionsGenerator():
+    # << Predictions Generator >>
+    def __init__(self, fileName):
+        self.file = fileName
+        self.probabilities = self.generateProbabilities()
+        
+    @abstractmethod
+    def generateProbabilities(self):
+        pass
+
+class KagglePredictionsGenerator(PredictionsGenerator):
+    # Concrete for <<PredictionsGenerator>>
+    # KNOW: Kaggle Input Data
+    # Do: Produce dictionary to be passed to be GamePredictions class
+        # key              :   value
+        # (teamid, teamid) :   0.5
+    def __init__(self, fileName):
+        super().__init__(fileName)
+        
+    def generateProbabilities(self):
+        probDictionary = {}
+        with open(self.file, 'r') as file:
+            for num, row in enumerate(file):
+                if num > 0:
+                    team1 = row[5:9]
+                    team2 = row[10:14]
+                    prob = row[15:]
+                    probDictionary[(team1, team2)] = float(prob)
+        return probDictionary    
+
+class Predictions():
+    def __init__(self):
+        self._predictions = self.initializePredictions()
+    
+    def initializePredictions(self):
+        generator = KagglePredictionsGenerator('testPredictions.csv')
+        return generator.probabilities
+
+    @property
+    def predictions(self):
+        return self._predictions
+    
+    @predictions.setter
+    def predictions(self, value):
+        print("Sorry, can't reset the predictions!")
+        return
+    
+class Game():
+    def __init__(self, team1, team2):
+        self.team1 = team1
+        self.team2 = team2
+
+        if self.team1 > self.team2:
+            self.team1, self.team2 = self.team2, self.team1
+        
+    def _getPredictions(self, predictionsObject):
+        return predictionsObject.predictions
+    
+    def _getTeamOneWinPctWith(self, predictionsObject):
+        predictions = self._getPredictions(predictionsObject)
+        return predictions[(self.team1, self.team2)]
+
+    def simulateWith(self, predictionsObject):
+        team1wins = self._getTeamOneWinPctWith(predictionsObject)
+        val = np.random.uniform(0, 1)
+        #print(val)
+        if val < team1wins:
+            return self.team1
+        else:
+            return self.team2
+    
+    def __str__(self):
+        return f"{self.team1} vs. {self.team2}"
+
+    def __repr__(self):
+        return f"{self.team1} vs. {self.team2}"
+        
+class BracketEntry():
+    def __init__(self, index):
+        self.index = index
+        self.team1 = None
+        self.team2 = None
+        self._game = None
+
+    def addTeam(self, team):
+        if self.team1 is None:
+            self.team1 = team
+        elif self.team2 is None:
+            if team < self.team1:
+                self.team2 = self.team1
+                self.team1 = team
+            else:
+                self.team2 = team
+        else:
+            print("Woops, no more teams can be added to this game!")
+    
+    @property
+    def game(self):
+        if self._game is not None:
+            return self._game
+        elif self.team1 is not None and self.team2 is not None:
+            self._game = Game(self.team1, self.team2)
+            return self._game
+        else:
+            return
+            
+    @game.setter
+    def game(self):
+        print("The BracketEntry's game property cannot be reset! Please add teams to entry to instantiate a game!")
+        return
+
+    def getWinner(self, predictionsObject):
+        return self.game.simulateWith(predictionsObject)
+    
+    def getNextGameIndex(self):
+        return self.index // 2
+
+    def __str__(self):
+        return f"{self.index}: {self.game}"
+    
+    def __repr__(self):
+        return f"{self.index}: {self.game}"
+
+class Bracket():
+    def __init__(self, size, predictions):
+        self.size = size
+        self.gameBracket = [None] * size
+        self.winnerBracket = [None] * size
+        self.randomlyFillFirstRound()
+        self.predictions = predictions
+    
+    def assignFirstRound(self, someBracketTemplate):
+        self.randomlyFillFirstRound()
+        #TODO: Implement this and eventually replace randomlyFillFirstRound
+
+    def randomlyFillFirstRound(self):
+        # TODO: Update with deterministic bracket once it's set
+        # i.e., shouldn't be doing any shuffling to determine first round
+        preds = Predictions().predictions
+        listOfTeams = list(set([i[0] for i in preds.keys()]))[:64]
+        random.shuffle(listOfTeams)
+        for num, i in enumerate(range(32, 64)):
+            idx = i - 32
+            teamA = listOfTeams[2 * idx]
+            teamB = listOfTeams[2 * idx + 1]
+            bracketEntry = BracketEntry(i)
+            bracketEntry.addTeam(teamA)
+            bracketEntry.addTeam(teamB)
+            self.gameBracket[i] = bracketEntry
+        
+    @property
+    def round(self):
+        ctr = 0
+        for num, i in enumerate(self.gameBracket):
+            if i is not None:
+                return math.log(self.size // ctr, 2)
+            ctr += 1
+    
+    @round.setter
+    def round(self, value):
+        print("Round is an internally-determined property and cannot be reset!")
+        return
+
+    def _simulateRound(self):
+        thisRoundSize = self.size / (2 ** self.round) 
+        
+        if (thisRoundSize == int(thisRoundSize) and (thisRoundSize % 2 == 0)) or (thisRoundSize == 1):
+            thisRoundSize = int(thisRoundSize)
+        else:
+            print("Hmmm, the size of this round should be an even integer!")
+            raise ValueError
+                
+        for gameIndex in range(thisRoundSize, thisRoundSize * 2):
+            bracketEntry = self.gameBracket[gameIndex]
+            
+            bracketEntryWinner = bracketEntry.getWinner(self.predictions)
+            bracketEntryNextIndex = bracketEntry.getNextGameIndex()
+            self.winnerBracket[gameIndex] = bracketEntryWinner 
+            
+            if thisRoundSize > 1:
+                self._insertBracketEntry(bracketEntryNextIndex)
+                nextEntry = self.gameBracket[bracketEntryNextIndex]
+                nextEntry.addTeam(bracketEntryWinner)
+            
+    def _insertBracketEntry(self, index):
+        if self.gameBracket[index] is None:
+            self.gameBracket[index] = BracketEntry(index)
+
+    def simulateTournament(self, reset = True):
+        while self.winnerBracket[1] is None:
+            self._simulateRound()
+        if reset:
+            self.bracketReset()
+    
+    def bracketReset(self):
+        self.gameBracket = [None] * self.size
+        self.winnerBracket = [None] * self.size
+        self.randomlyFillFirstRound()
+        
+preds = Predictions()
+test = Bracket(64, preds)
+test.randomlyFillFirstRound()
+test.simulateTournament(reset = False)
+print(test.winnerBracket)
